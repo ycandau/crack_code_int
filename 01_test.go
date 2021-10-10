a5b3c2166fa141d0a1cddfa07e77242992dd1d74
Yves Candau <ycandau@sfu.ca>
2021-10-10 00:30:55 -0700

    Add tests for Problem 1.2

--- 01_test.go
+++ 01_test.go
@@ -3,13 +3,14 @@ package main
 import "testing"
 
 //------------------------------------------------------------------------------
+// Problem 1.1
 
-type addTest struct {
-	str      string
+type addTest1 struct {
+	str string
 	exp bool
 }
 
-var testsAllUnique = []addTest{
+var testsAllUnique = []addTest1{
 	{"abcde", true},
 	{"abcda", false},
 	{"ebcde", false},
@@ -30,6 +31,35 @@ func TestAllUniqueWithoutMap(t *testing.T) {
 	for _, test := range testsAllUnique {
 		if output := AllUniqueWithoutMap(test.str); output != test.exp {
 			t.Errorf("( %v ) => %v | %v", test.str, output, test.exp)
+		} else {
+			t.Log("Pass")
+		}
+	}
+}
+
+//------------------------------------------------------------------------------
+// Problem 1.2
+
+type addTest2 struct {
+	str1 string
+	str2 string
+	exp  bool
+}
+
+var testsArePermutations = []addTest2{
+	{"xabc", "xabc", true},
+	{"xabc", "abcx", true},
+	{"xabc", "abc", false},
+	{"abc", "xabc", false},
+	{"aabc", "abcc", false},
+	{"xabc", "abcy", false},
+}
+
+func TestArePermutations(t *testing.T) {
+	for _, test := range testsArePermutations {
+		if output := ArePermutations(test.str1, test.str2); output != test.exp {
+			t.Errorf("( %v , %v ) => %v | %v",
+				test.str1, test.str2, output, test.exp)
 		} else {
 			t.Log("Pass")
 		}
