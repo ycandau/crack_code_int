ffd2739d6df7fb14b8afa1623ff464239dfc9e8c
Yves Candau <ycandau@sfu.ca>
2021-10-15 23:09:48 -0700

    Add solution for Problem 5.1

--- 05_bit_manipulation.go
+++ 05_bit_manipulation.go
@@ -3,7 +3,10 @@ package main
 //------------------------------------------------------------------------------
 // Problem 5.1
 
-func Insertion(dest, src, pos int) int {
+func Insertion(dest, src, begin, end int) int {
+	mask := 1 << (end - begin + 1) // 0000 1000
+	mask -= 1 // 0000 0111
+	mask <<= begin // 0001 1100
 
-	return dest
+	return (dest &^ mask) | (src << begin)
 }
--- 05_test.go
+++ 05_test.go
@@ -10,23 +10,28 @@ import (
 type testType51 struct {
 	dest int
 	src int
-	pos int
+	begin int
+	end int
 	exp int
 }
 
 var tests51 = []testType51{
-	{0, 0, 0, 0},
-	{0, 7, 0, 7},
-	{0, 7, 2, 27},
+	{0, 0, 0, 2, 0},
+	{255, 0, 0, 2, 255 - 7},
+	{255, 0, 2, 4, 255 - 28},
+	{0, 7, 0, 5, 7},
+	{0, 7, 2, 7, 28},
+	{255, 4, 0, 3, 255 - 11},
+	{255, 4, 2, 5, 255 - 44},
 
 }
 
 func Test51(t *testing.T) {
 	for _, test := range tests51 {
-		output := Insertion(test.dest, test.src, test.pos)
+		output := Insertion(test.dest, test.src, test.begin, test.end)
 		if output != test.exp {
-			t.Errorf("( %v %v %v ) => %v | %v",
-				test.dest, test.src, test.pos, output, test.exp)
+			t.Errorf("( %v %v %v %v ) => %v | %v",
+				test.dest, test.src, test.begin, test.end, output, test.exp)
 		} else {
 			t.Log("Pass")
 		}
